c1b1a96758be58054b2b8ea4a36cbf6709a61e54
AurekBrillowski <aurek@parkcitiesxc.org>
2019-01-16 22:06:03 -0600

    DEPLOY TEST - Modified Background 02

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body style="background-color: #F5Ffff;">
+<body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
